19a1c8558fc074934407785f27b9c5c5f09eb625
Silas L. Pascoal <slpascoal01@gmail.com>
2024-09-14 12:36:46 -0300

    Update UserController.php

--- simulador-empregabilidade/app/Http/Controllers/UserController.php
+++ simulador-empregabilidade/app/Http/Controllers/UserController.php
@@ -8,29 +8,36 @@ use GuzzleHttp\Client;
 class UserController extends Controller
 {
     public function index(Request $request)
-    {
-        $client = new Client([
-            'verify' => false
-        ]);
+{
+    $client = new Client([
+        'verify' => false
+    ]);
 
-        $response = $client->get('https://random-data-api.com/api/v2/users?size=100');
-        $users = json_decode($response->getBody()->getContents(), true);
+    $response = $client->get('https://random-data-api.com/api/v2/users?size=100');
+    $users = json_decode($response->getBody()->getContents(), true);
 
-        // Extrair habilidades únicas
-        $skills = array_unique(array_column(array_column($users, 'employment'), 'key_skill'));
+    // Extrair habilidades únicas
+    $skills = array_unique(array_column(array_column($users, 'employment'), 'key_skill'));
 
-        if ($request->has('key_skill')) {
-            $users = array_filter($users, function($user) use ($request) {
-                return $user['employment']['key_skill'] == $request->key_skill;
-            });
-        }
+    // Mensagens de depuração
+   // dd($request->all(), $skills); // Verifique os parâmetros de filtro e as habilidades
 
-        if ($request->has('title')) {
-            $users = array_filter($users, function($user) use ($request) {
-                return stripos($user['employment']['title'], $request->title) !== false;
-            });
-        }
-
-        return view('users.index', compact('users', 'skills'));
+    // Aplicar filtros
+    if ($request->has('key_skill') && $request->key_skill !== '') {
+        $users = array_filter($users, function($user) use ($request) {
+            return $user['employment']['key_skill'] === $request->key_skill;
+        });
     }
+
+    if ($request->has('title') && $request->title !== '') {
+        $users = array_filter($users, function($user) use ($request) {
+            return stripos($user['employment']['title'], $request->title) !== false;
+        });
+    }
+
+    //dd($users); // Verifique os dados filtrados
+
+    return view('users.index', compact('users', 'skills'));
+}
+
 }
