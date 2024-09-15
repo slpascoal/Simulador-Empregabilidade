e4ddfeb072b934777267e297c13efb556b0a534f
Silas L. Pascoal <slpascoal01@gmail.com>
2024-09-15 18:52:03 -0300

    Atualização das Rotas

--- simulador-empregabilidade/routes/web.php
+++ simulador-empregabilidade/routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
+// Defina a rota para '/' e dê o nome 'users.index'
+Route::get('/', [UserController::class, 'index'])->name('users.index');
+
+// Mantenha a rota /users como um alias, se preferir
+Route::get('/users', [UserController::class, 'index']);
